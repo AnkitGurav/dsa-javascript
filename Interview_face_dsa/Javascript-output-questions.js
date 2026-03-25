10fbbf88f182eaeb5435e96b46a5fd7003f2cd96
Ankit Gurav <ankitgurav2511@gmail.com>
2026-03-25 16:45:49 +0530

    Todays React Interview Output Questions

--- Interview_face_dsa/Javascript-output-questions.js
+++ Interview_face_dsa/Javascript-output-questions.js
@@ -3,11 +3,11 @@
 console.log("Start");
 
 setTimeout(() => {
-  console.log("Timeout");
+    console.log("Timeout");
 }, 0);
 
 Promise.resolve().then(() => {
-  console.log("Promise");
+    console.log("Promise");
 });
 
 console.log("End");
@@ -19,18 +19,29 @@ console.log("End");
 // Timeout
 
 //2.Write a function to return new array grater than 50
-let arr =[12,32,43,76,55,65,34,87,34,23]
+let arr = [12, 32, 43, 76, 55, 65, 34, 87, 34, 23]
 
 let number = 50;
 
-function GreaterThanFifty(arr, num){
+function GreaterThanFifty(arr, num) {
     let sampleArr = [];
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > num){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > num) {
             sampleArr.push(arr[i]);
         }
     }
     return sampleArr;
 }
 console.log(GreaterThanFifty(arr, number));
+
+// using filter method;
+let filterArray = arr.filter(item => item > number);
+console.log(filterArray)
+
+// using map + filter method;
+// map() is for transforming values, not filtering.
+// But if you still want:
+let newArray = arr.map(item => item > number ? item : null).filter((item) => item !== null);
+ console.log(newArray)
+
 
